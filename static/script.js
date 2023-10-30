e000ece947c3ec6a0dd5930e879f72a0dfdbbaa9
Ricardo Vaz <rivaz.claudio@gmail.com>
2023-10-30 14:16:31 -0300

    portfolio, sobre e contato

--- static/script.js
+++ static/script.js
@@ -8,3 +8,17 @@ menuMobile.addEventListener('click', ()=> {
         : menuMobile.classList.replace("bi-x","bi-list");
     body.classList.toggle("menu-nav-active");
 });
+
+
+/*fecha o menu quando clicar quando clicar em algum item e  o muda o ícone para list */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item =>{
+    item.addEventListener("click",() => {
+        if (body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x","bi-list");
+        }
+    }) 
+})
